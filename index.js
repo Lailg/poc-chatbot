06d4a272f3700b742dc00ea5029ee687cc5586b0
Lailg <lailg.hedspi@gmail.com>
2025-03-25 17:48:04 +0700

    add root endpoint

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 // Webhook xác thực
 app.get("/webhook", (req, res) => {
     const mode = req.query["hub.mode"];
